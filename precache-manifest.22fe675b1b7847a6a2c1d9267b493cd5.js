d614e0bec5d7fbd49a72b125266fc0ae542c2375
CavaiNice <abcstud@inbox.ru>
2020-09-07 22:52:38 +0300

    Updates

---
+++ precache-manifest.22fe675b1b7847a6a2c1d9267b493cd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3bd9993a114e74549f9f10d52b283a",
+    "url": "/didactic-octo-enigma/index.html"
+  },
+  {
+    "revision": "ac80d214328f18eed644",
+    "url": "/didactic-octo-enigma/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/didactic-octo-enigma/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/didactic-octo-enigma/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac80d214328f18eed644",
+    "url": "/didactic-octo-enigma/static/js/main.99df5a71.chunk.js"
+  },
+  {
+    "revision": "462cc576c88f1096f221",
+    "url": "/didactic-octo-enigma/static/js/runtime-main.b50a776c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/didactic-octo-enigma/static/media/logo.5d5d9eef.svg"
+  }
+]);
